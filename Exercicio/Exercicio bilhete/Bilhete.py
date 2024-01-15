5f33051ca7bd44b0c20a65ae7ca5e275ae7befeb
Danilo Andrade <danilo16pereira@gmail.com>
2024-01-15 17:08:39 -0300

    Tesouro

--- Exercicio/Exercicio bilhete/Bilhete.py
+++ Exercicio/Exercicio bilhete/Bilhete.py
@@ -11,13 +11,17 @@ if altura >= 1.20:
     print("-="*15)
     print('\nInfantil - R$ 5 (abaixo de 12)\nMeia     - R$ 7 (abaixo de 12)\nAdulto   - R$ 12 (abaixo de 12)\n')
     bilhe = int(input("Digite a sua idade:"))
-    if bilhe <= 12:
+    if bilhe <= 12 :
         conta = 5
         print('Bilhete Infantil')
     elif bilhe <=18:
         conta = 7
         print('Bilhete Meia')
-    else :
+
+    elif bilhe >= 45 and bilhe <=55:
+        conta = 0
+        print("Entrada Franca")
+    else:
         conta = 12
         print('Bilhete Adulto')
 
@@ -25,7 +29,9 @@ if altura >= 1.20:
     foto = str(input("Vai querer uma foto do momento? S ou N :"))
     if foto == 's' or foto == 'S':
         conta += 3
-    print(f"O total e: R${conta}")
+    print(f"\nO total e: R${conta}")
+    
+
 
 else:
     print('Por Segurança, voce não atenda a altura desejada')
