d9e51ea8a508d1601db8ae0dd004f18c504f6a48
Danilo Andrade <danilo16pereira@gmail.com>
2024-01-17 17:50:38 -0300

    Quem Paga

---
+++ Exercicio/QuemPaga/Quempaga.py
@@ -0,0 +1,10 @@
+import random
+import time
+
+amigos_nome = random.choice(['Danilo','Lucas','Gabriel','Vitor','Samuel','Derek','Wili','Wilian','Ilton','Zé Maria','Andersom','Pedro Paulo'])
+
+print('Quem vai pagar a conta hoje vai ser!!!!')
+
+time.sleep(2)
+
+print(f'Parabens, {amigos_nome} você vai pagar toda a conta da noite')
