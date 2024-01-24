2584df10a44bc24a3b4a3cd2eca734fc9d84d68e
Danilo Andrade <danilo16pereira@gmail.com>
2024-01-24 11:35:08 -0300

    Exercicio Loop altura

--- Exercicio/Alturacode/altura.py
+++ Exercicio/Alturacode/altura.py
@@ -1,5 +1,23 @@
-pes = [250,500,450]
 
-for altura in pes:
-    tamanho = 60 /pes
-    print (tamanho)
+aluno = input("Digite as alturas:").split()
+
+for n in range(0, len(aluno)):
+    aluno[n] = int(aluno[n])
+
+pes = 0
+
+for lop in aluno:
+    pes += lop 
+print(f"Soma da altura {pes}")
+
+cont = 0
+
+for lop in aluno:
+    cont += 1
+print(f"Pessoas registradas {cont}")
+
+media = 0
+
+for lop in aluno:
+    media =  pes / cont
+print(f"Pessoas registradas {media}")
