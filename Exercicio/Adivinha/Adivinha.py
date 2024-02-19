2dcf76236cb2f91dcabe98a2413494ca7d64ce18
Danilo Andrade <danilo16pereira@gmail.com>
2024-02-19 11:55:53 -0300

    Exercicio7-1ok

--- Exercicio/Adivinha/Adivinha.py
+++ Exercicio/Adivinha/Adivinha.py
@@ -1,3 +1,13 @@
 import random;
 
- 
+lista = ['bolo','chocolate','biscoito']
+
+es = random.choice(lista)
+
+pessoa = input("Digite uma letra:").lower()
+
+for var in es:
+    if var == pessoa:
+        print("Acerteu!")
+    else:
+        print("Errou!")
